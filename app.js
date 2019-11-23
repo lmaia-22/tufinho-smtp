1da99a33191c5979e726c82a6e0a5e9192b18551
lmaia-22 <luismsm14@gmail.com>
2019-11-23 12:46:28 +0000

    smtp

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+const transporter = nodemailer.createTransport({
+
+  host: 'smtp.gmail.com',
+  provider: 'gmail',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'tufinho.wish@gamil.com', // Enter here email address from which you want to send emails
+    pass: 'tufinho_123' // Enter here password for email account from which you want to send emails
+  },
+  tls: {
+  rejectUnauthorized: false
+  }
+});
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.status(200);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
+  next();
+});
+
+app.post('/send', function (req, res) {
+
+  let senderName = 'Gata';
+  let senderEmail = req.body.contactFormEmail;
+  let messageSubject = req.body.contactFormSubjects;
+  let messageText = req.body.contactFormMessage;
+  let copyToSender = req.body.contactFormCopy;
+
+  let mailOptions = {
+    to: ['luismsms14@gmail.com'], // Enter here the email address on which you want to send emails from your customers
+    from: senderName,
+    subject: messageSubject,
+    text: "Email cliente: " + senderEmail + 
+    "\nMensagem:" + messageText,
+    replyTo: senderEmail
+  };
+
+  if (senderName === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (senderEmail === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (messageSubject === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (messageText === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (copyToSender) {
+    mailOptions.to.push(senderEmail);
+  }
+
+  transporter.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error);
+      res.end('error');
+    } else {
+      console.log('Message sent: ', response);
+      res.end('sent');
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log('Express started on port: ', port);
+});
