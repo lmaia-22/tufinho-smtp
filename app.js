fd2c084184d755ca7977c5395951f94b4aef94c0
lmaia-22 <luismsm14@gmail.com>
2020-02-24 15:14:51 +0000

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 let cron = require('node-cron');
 var rp = require('request-promise');
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
