349f82de67de10a0e0e989c510861ed4af67f642
lmaia-22 <luismsm14@gmail.com>
2020-02-24 11:53:28 +0000

    9am send email

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.post('/kitty', function (req, res) {
 
 app.listen(port, function () {
   console.log('Express started on port: ', port);
-  cron.schedule('* * * * *', () => {
+  cron.schedule('0 9 * * *', () => {
 
     Url = 'https://smtp-tufinho.herokuapp.com/kitty';
 
